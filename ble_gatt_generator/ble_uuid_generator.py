39c2b1d0fa527e234eb619bf80f994ca76e5fad5
truonghoangduy <truonghoangduy03111999@gmail.com>
2022-12-14 11:47:52 +0700

    add: dart OBD Enum BLE Gatt SwitchCase

--- ble_gatt_generator/ble_uuid_generator.py
+++ ble_gatt_generator/ble_uuid_generator.py
@@ -1,6 +1,8 @@
 #! /usr/local/bin/python3
 import yaml
 import os
+import itertools
+
 
 pathTOHeaderFile = "./c/BLE_DESIGN.g.h"
 # pathToSerialDebugTool = "./c/BLE_DESIGN.g.h"
@@ -20,6 +22,7 @@ def read_config():
 
 
 def cHeaderFileLimbo(listOfData):
+
     deviceNameDefine = '''#ifndef DEVICE_NAME\n#define DEVICE_NAME "{value}"\n#endif\n'''.format(
         value=deviceName)
     output = ""
@@ -39,17 +42,81 @@ def dartCode(listOfData):
         "\nimport 'package:flutter_reactive_ble/flutter_reactive_ble.dart';\n// ignore_for_file: non_constant_identifier_names\n"
     servicesAsList = ""
     geneteuuid = ""
+    BLE_KEYS = []
     for i in listOfData:
         for key, value in i.items():
             text = '''Uuid {key} = Uuid.parse("{value}");
             '''.format(key=key, value=value)
+            BLE_KEYS.append(key)
             geneteuuid += text + "\n"
             if "SERVICE" in key:
                 servicesAsList += '''{key},'''.format(key=key)
 
     servicesUUIDasListExport = '''List<Uuid> deviceServices = [{services}];'''.format(
         services=servicesAsList)
-    return output+geneteuuid+"\n"+servicesUUIDasListExport+"\n"+deviceNameDartCode
+    ble_gatt_generated = output+geneteuuid+"\n" + \
+        servicesUUIDasListExport+"\n"+deviceNameDartCode
+    ble_gatt_generated += "\n"
+    dartEnumList = []
+    for text in BLE_KEYS:
+        capitalLifeText = ["".join(x.capitalize()) for x in text.split('_')]
+        joinedText = "".join(capitalLifeText)
+        removeBLEText = joinedText.replace("Ble", "")
+        lowerCaseFristLetter = removeBLEText[0].lower() + removeBLEText[1:]
+        dartEnumList.append(lowerCaseFristLetter)
+    # print(dartEnumList)
+    dartEnumList = [x for x in dartEnumList if "service" not in x]
+    ble_obd_enum = '''enum BleOBDCheckList ''' + \
+        "{" + '''{ble_name}'''.format(ble_name=",".join(dartEnumList)) + "}"
+    ble_gatt_generated += ble_obd_enum
+
+    # ble_gatt_char array
+    characteristic_uuid = list(filter(
+        lambda item: ("SERVICE" not in item), BLE_KEYS))
+    characteristic_uuid_keys = ""
+    for uuid in characteristic_uuid:
+        characteristic_uuid_keys += '''{key},'''.format(key=uuid)
+    ble_gatt_char = '''List<Uuid> deviceCharacteristic = [{list}];'''.format(
+        list=characteristic_uuid_keys)
+    ble_gatt_generated += "\n"
+    ble_gatt_generated += ble_gatt_char
+    # # gatt_map
+    # ble_gatt_service = {}
+    # for i in listOfData:
+    #     for key, value in i.items():
+    #         if "SERVICE" in key:
+    #             ble_gatt_service[key] = []
+    #         # if "ENGINE"
+
+    # dartMap = '''Map<Uuid,List<Uuid>> BLE_MAP = {
+    #     {data}
+    # }'''.format(data="")
+    # print(characteristic_uuid_keys)
+    dartOBDEnumBLEGattSwitchCaseText = dartOBDEnumBLEGattSwitchCase(
+        dartEnumList, characteristic_uuid)
+    ble_gatt_generated += dartOBDEnumBLEGattSwitchCaseText
+    return ble_gatt_generated
+
+
+def dartOBDEnumBLEGattSwitchCase(listObdEnum, listBle_gatt_uuid):
+    listOfCase = []
+    for obdEnum, ble_gatt_uuid in zip(listObdEnum, listBle_gatt_uuid):
+        ifStatement = '''
+        if (characteristicId == {ble_gatt_uuid}) {{
+            return BleOBDCheckList.{obdEnum};
+        }}
+        '''.format(ble_gatt_uuid=ble_gatt_uuid, obdEnum=obdEnum)
+        listOfCase.append(ifStatement)
+        # print(obdEnum, ble_gatt_uuid)
+
+    template = '''
+    BleOBDCheckList getOBDEnmum(Uuid characteristicId) {{
+        {cases}
+        return BleOBDCheckList.engineAirIntakeTempCharacteristic;
+    }}
+    '''.format(cases="\n".join(listOfCase))
+    # print("\n".join(listOfCase))
+    return template
 
 
 def generate(fileGenrateCallback, fileLocation):
